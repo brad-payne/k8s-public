586ab32047792dfd573d1f904f82c5add2b344d8
Brad Payne <brad.payne@gmail.com>
2019-07-17 11:36:15 -0400

    commented out unnecessary apt update from k8s-install.sh file

--- k8s-cluster-installation/k8s-install.sh
+++ k8s-cluster-installation/k8s-install.sh
@@ -5,7 +5,7 @@
 #sudo -i
 #curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 #echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >> /etc/apt/sources.list.d/kubernetes.list
-apt update
+#sudo apt update
 
 ##apt install -y kubelet kubeadm kubectl
 #kubeadm init --pod-network-cidr=10.244.0.0/16  # Flannel pod network, see below
